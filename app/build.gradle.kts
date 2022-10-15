11118c6a70fca0944fa10572c691af0ee9d3969f
yurzhang <yurzhang@163.com>
2022-10-15 14:53:51 +0800

    chore: styled code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,29 +7,29 @@
  */
 
 plugins {
-    // Apply the application plugin to add support for building a CLI application in Java.
-    application
+  // Apply the application plugin to add support for building a CLI application in Java.
+  application
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
-    mavenCentral()
+  // Use Maven Central for resolving dependencies.
+  mavenCentral()
 }
 
 dependencies {
-    // Use JUnit Jupiter for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+  // Use JUnit Jupiter for testing.
+  testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 
-    // This dependency is used by the application.
-    implementation("com.google.guava:guava:31.0.1-jre")
+  // This dependency is used by the application.
+  implementation("com.google.guava:guava:31.0.1-jre")
 }
 
 application {
-    // Define the main class for the application.
-    mainClass.set("oop.homework2.App")
+  // Define the main class for the application.
+  mainClass.set("oop.homework2.App")
 }
 
 tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
-    useJUnitPlatform()
+  // Use JUnit Platform for unit tests.
+  useJUnitPlatform()
 }
